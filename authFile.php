efb8ff38b95f965df507c99ddf688e0ef48cfdb8
gERYHWeb <geryh213921@gmail.com>
2016-07-28 18:31:29 +0700

    deleted

--- authFile.php
+++
@@ -1,6 +0,0 @@
-<?
-require($_SERVER['DOCUMENT_ROOT']."/bitrix/header.php");
-$USER->Authorize(1); // укажите ID вашего пользователя
-LocalRedirect('/bitrix/admin/');
-require($_SERVER['DOCUMENT_ROOT']."/bitrix/footer.php");
-?>
