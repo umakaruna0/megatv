0ec6c31b36bb469adb331a6a7ce1c8aa97804836
gERYHWeb <geryh213921@gmail.com>
2016-07-28 19:55:44 +0700

    changes into components (template.php)

--- local/templates/megatv/components/bitrix/menu/top/template.php
+++ local/templates/megatv/components/bitrix/menu/top/template.php
@@ -22,11 +22,7 @@ foreach($arResult as $key=>$arItem):
     
 <?endforeach?>
     <?if($showMyRecs):?>
-<<<<<<< HEAD
         <a class="box-menu__link menu-link<?if($arItem["SELECTED"]):?> menu-link--active<?endif;?> item-recording" href="/personal/records/"><span data-icon="icon-film-collection" class="menu-link__icon g-icon"></span><span class="box-menu__title"><?=$myRecSelected["TEXT"]?><span class="item-recording__count"><?=$APPLICATION->GetPageProperty("ar_record_in_rec")?> <?=GetMessage('FROM')?> <?=$APPLICATION->GetPageProperty("ar_record_total")?></span> Мои записи</span></a>
-=======
-        <li class="<?if($myRecSelected["SELECTED"]):?>active <?else:?><?endif;?>sections-menu-recordings"><a href="/personal/records/"><span data-icon="icon-film-collection"></span> <span><?=$myRecSelected["TEXT"]?><span class="count"><?=$APPLICATION->GetPageProperty("ar_record_in_rec")?> <?=GetMessage('FROM')?> <?=$APPLICATION->GetPageProperty("ar_record_total")?></span></span></a></li>
->>>>>>> 23cd50036497da64b44e0bc4c24882eeb021600e
     <?endif;?>
 </div>
 <?endif?>
