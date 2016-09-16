f65f89d2edc988c2108df5e703c88d87e14ae1f9
Евгений Генов < >
2016-09-16 17:34:12 +0700

    post final commit

--- local/templates/megatv/tmpl/index.php
+++ local/templates/megatv/tmpl/index.php
@@ -67,19 +67,20 @@
 <script id="paramsJson" type="text/json"><? include "/js/json_array.js"; ?></script>
 
 <script id="broadcastTmpl" type="text/x-template">
-    <div class="bs-container__broadcast<% if(!noAir){ %> status-recordable <% } %> broadcast<% if(noAir){ %> broadcast--no-air<% } %>" data-type="broadcast" data-broadcast-id="<%- id %>">
+    <div class="bs-container__broadcast broadcast<% if(noAir && auth){ %> broadcast--no-air<% } %>" data-type="broadcast" data-broadcast-id="<%- id %>">
         <% if(onAir){ %>
         <span data-channel-id="<%- channel_id %>" class="badge broadcast__on-air">В эфире</span>
         <% } %>
         <img class="broadcast__image broadcast-image lazy-img swiper-lazy" data-src="<%- blurImage %>" data-load="<%- image %>">
-        <% if(!noAir){ %>
+        
         <div class="broadcast__wrap-status">
+        <% if(!noAir){ %>
             <span class="broadcast__status item-status-icon">        
                 <div data-icon="icon-recordit"></div>
                 <span class="bs-status__title">Записать</span>
             </span>
-        </div>
         <% } %>
+        </div>
         <div class="broadcast__info item-header">
             <div class="broadcast__time broadcast-time"><%- time %></div>
             <div class="broadcast__title broadcast-title">
