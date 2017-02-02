cebb1b3eda2f6fe7b3d9b966713d49c4e9582699
Kremonit <hawkart@rambler.ru>
2017-02-02 21:18:35 +0300

    update generate cell

--- cron/generate_cell.php
+++ cron/generate_cell.php
@@ -13,19 +13,6 @@ global $USER, $APPLICATION;
 if (!is_object($USER))
     $USER=new \CUser;
 
-\CDev::deleteDirectory($_SERVER['DOCUMENT_ROOT'].'/bitrix/cache', 0);
-
-\CDev::deleteDirectory($_SERVER["DOCUMENT_ROOT"]."/upload/cell/", 86400*2);
-
-$arCities = \Hawkart\Megatv\CityTable::getLangCityList(15); //RU
-foreach($arCities as $arCity)
-{
-    $fisrt_date = date('d.m.Y', strtotime(\CTimeEx::getCurDate()));
-    for($day=0; $day<3; $day++)
-    {
-        $curDate = date('d.m.Y', strtotime("+".$day." day", strtotime($fisrt_date)));
-        \Hawkart\Megatv\ScheduleCell::generate($curDate, $arCity["ID"]);
-    }
-}
+//\Hawkart\Megatv\ScheduleCell::generateForWeek();
 die();
 ?>
--- cron/import_epg.php
+++ cron/import_epg.php
@@ -28,6 +28,8 @@ while ($row = $result->fetch())
 }
 */
 
+mail("hawkart@rambler.ru", "Tvguru epg import success: ".$dstart, "start of import");
+
 //Удаляем старые файлы лога
 $path = $_SERVER['DOCUMENT_ROOT'].'/logs/sotal/';
 \CDev::deleteOldFiles($path, 86400);
@@ -46,18 +48,8 @@ $epg->import();
 
 \Hawkart\Megatv\ScheduleTable::slice(); 
 \CDev::deleteDirectory($_SERVER['DOCUMENT_ROOT'].'/bitrix/cache', 0);
-\CDev::deleteDirectory($_SERVER["DOCUMENT_ROOT"]."/upload/cell/", 86400*2);
 
-$arCities = \Hawkart\Megatv\CityTable::getLangCityList(15); //RU
-foreach($arCities as $arCity)
-{
-    $fisrt_date = date('d.m.Y', strtotime(\CTimeEx::getCurDate()));
-    for($day=0; $day<3; $day++)
-    {
-        $curDate = date('d.m.Y', strtotime("+".$day." day", strtotime($fisrt_date)));
-        \Hawkart\Megatv\ScheduleCell::generate($curDate, $arCity["ID"]);
-    }
-}
+\Hawkart\Megatv\ScheduleCell::generateForWeek();
 
 \CDev::deleteDirectory($_SERVER['DOCUMENT_ROOT'].'/bitrix/cache', 0);
 echo $dfinish = date("H:i:s")."\r\n";
